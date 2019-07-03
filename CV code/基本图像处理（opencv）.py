faf9e2d1928e210880d9c9d20455bd4dea7afe1e
duchp <48784983+duchp@users.noreply.github.com>
2019-07-03 10:27:34 +0800

    Add files via upload

---
+++ CV code/基本图像处理（opencv）.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul  1 09:52:25 2019
+
+@author: Du Chenping
+"""
+
+import cv2
+import random
+import numpy as np
+from matplotlib import pyplot as plt
+
+#img show
+img = cv2.imread('cat.jpg')
+cv2.imshow('cat', img)
+print(img.shape)
+
+#img crop
+img_crop = img[100:300, 300:500]
+cv2.imshow('img_crop', img_crop)
+
+#img color shift
+def random_light_color(img):
+    # brightness
+    B, G, R = cv2.split(img)
+
+    b_rand = random.randint(-50, 50)
+    if b_rand == 0:
+        pass
+    elif b_rand > 0:
+        lim = 255 - b_rand
+        B[B > lim] = 255
+        B[B <= lim] = (b_rand + B[B <= lim]).astype(img.dtype)
+    elif b_rand < 0:
+        lim = 0 - b_rand
+        B[B < lim] = 0
+        B[B >= lim] = (b_rand + B[B >= lim]).astype(img.dtype)
+
+    g_rand = random.randint(-50, 50)
+    if g_rand == 0:
+        pass
+    elif g_rand > 0:
+        lim = 255 - g_rand
+        G[G > lim] = 255
+        G[G <= lim] = (g_rand + G[G <= lim]).astype(img.dtype)
+    elif g_rand < 0:
+        lim = 0 - g_rand
+        G[G < lim] = 0
+        G[G >= lim] = (g_rand + G[G >= lim]).astype(img.dtype)
+
+    r_rand = random.randint(-50, 50)
+    if r_rand == 0:
+        pass
+    elif r_rand > 0:
+        lim = 255 - r_rand
+        R[R > lim] = 255
+        R[R <= lim] = (r_rand + R[R <= lim]).astype(img.dtype)
+    elif r_rand < 0:
+        lim = 0 - r_rand
+        R[R < lim] = 0
+        R[R >= lim] = (r_rand + R[R >= lim]).astype(img.dtype)
+
+    img_merge = cv2.merge((B, G, R))
+    #img = cv2.cvtColor(final_hsv, cv2.COLOR_HSV2BGR)
+    return img_merge
+
+img_random_color = random_light_color(img)
+cv2.imshow('img_random_color', img_random_color)
+
+#img rotation
+M = cv2.getRotationMatrix2D((img.shape[1] / 2, img.shape[0] / 2), 25, 0.5) # center, angle, scale
+img_rotate = cv2.warpAffine(img, M, (img.shape[1], img.shape[0]))
+cv2.imshow('rotated cat', img_rotate)
+
+#img perspective transform
+def random_warp(img, row, col):
+    height, width, channels = img.shape
+
+    # warp:
+    random_margin = 60
+    x1 = random.randint(-random_margin, random_margin)
+    y1 = random.randint(-random_margin, random_margin)
+    x2 = random.randint(width - random_margin - 1, width - 1)
+    y2 = random.randint(-random_margin, random_margin)
+    x3 = random.randint(width - random_margin - 1, width - 1)
+    y3 = random.randint(height - random_margin - 1, height - 1)
+    x4 = random.randint(-random_margin, random_margin)
+    y4 = random.randint(height - random_margin - 1, height - 1)
+
+    dx1 = random.randint(-random_margin, random_margin)
+    dy1 = random.randint(-random_margin, random_margin)
+    dx2 = random.randint(width - random_margin - 1, width - 1)
+    dy2 = random.randint(-random_margin, random_margin)
+    dx3 = random.randint(width - random_margin - 1, width - 1)
+    dy3 = random.randint(height - random_margin - 1, height - 1)
+    dx4 = random.randint(-random_margin, random_margin)
+    dy4 = random.randint(height - random_margin - 1, height - 1)
+
+    pts1 = np.float32([[x1, y1], [x2, y2], [x3, y3], [x4, y4]])
+    pts2 = np.float32([[dx1, dy1], [dx2, dy2], [dx3, dy3], [dx4, dy4]])
+    M_warp = cv2.getPerspectiveTransform(pts1, pts2)
+    img_warp = cv2.warpPerspective(img, M_warp, (width, height))
+    return M_warp, img_warp
+M_warp, img_warp = random_warp(img, img.shape[0], img.shape[1])
+cv2.imshow('cat_warp', img_warp)
+
+key = cv2.waitKey()
+if key == 27:
+    cv2.destroyAllWindows()
